3a52f22fc9f26e97110c08fe1cb44aba31afc1c7
caxton-m <caxymuchono@gmail.com>
2023-03-22 11:36:49 -0500

    first m

---
+++ R/myf.R
@@ -0,0 +1,15 @@
+# make a test function
+#' Square function
+#'
+#' Takes a vector and returns a vector of sqaures
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#'  x=1:30; myf(x)
+myf = function(x) {
+  x^2
+}
